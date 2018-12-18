71a1ee86ae57ce8e1465f96f43989807ebb6f893
Vamsi Tallapudi <vamsi@coderefer.com>
2018-12-18 09:37:38 +0530

    Initial commit

---
+++ app/src/main/java/com/coderefer/activitylaunchmodes/Activity4.kt
@@ -0,0 +1,12 @@
+package com.coderefer.activitylaunchmodes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_4)
+    }
+}
